a1fe58d99d821abf68f82e0a37f43c480b7c9e01
Jezmem <tomzarb98@gmail.com>
2026-01-29 12:00:19 +0800

    [US-1] Add Bottom Navigation

---
+++ lib/views/explore/explore.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExploreView extends StatelessWidget {
+  const ExploreView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Explore',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
