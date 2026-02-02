bf6a10b9253f5f2aa4a9b13f38794037b2468298
Jezmem <tomzarb98@gmail.com>
2026-02-02 19:30:32 +0800

    [US-3] Implement map + user location on the map

--- lib/views/explore/explore.dart
+++ lib/views/explore/explore.dart
@@ -1,15 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+import '../../core/services/explore/map_service.dart';
 
-class ExploreView extends StatelessWidget {
+class ExploreView extends StatefulWidget {
   const ExploreView({super.key});
 
   @override
+  State<ExploreView> createState() => _ExploreViewState();
+}
+
+class _ExploreViewState extends State<ExploreView> {
+  final MapService _mapService = MapService();
+  Position? _currentPosition;
+  bool _isLoading = true;
+  String? _errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    _initLocation();
+  }
+
+  Future<void> _initLocation() async {
+    try {
+      final position = await _mapService.getCurrentLocation();
+      if (mounted) {
+        setState(() {
+          _currentPosition = position;
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      if (mounted) {
+        setState(() {
+          _errorMessage = e.toString();
+          _isLoading = false;
+        });
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Explore',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      ),
+    if (_isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    if (_errorMessage != null) {
+      return Center(child: Text(_errorMessage!));
+    }
+
+    final LatLng initialCenter =
+        _currentPosition != null
+            ? LatLng(_currentPosition!.latitude, _currentPosition!.longitude)
+            : const LatLng(48.8566, 2.3522); // Default to Paris if null
+
+    return FlutterMap(
+      options: MapOptions(initialCenter: initialCenter, initialZoom: 15.0),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.hawkr',
+        ),
+        if (_currentPosition != null)
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: initialCenter,
+                width: 80,
+                height: 80,
+                child: const Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
+      ],
     );
   }
 }
